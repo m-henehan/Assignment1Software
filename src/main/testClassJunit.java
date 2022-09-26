b56bed6530439d6b48dc52a36dfdbe1c9fda1144
Maria Henehan <mhenehan999@gmail.com>
2022-09-26 16:06:42 +0100

    commit 2

--- src/main/testClassJunit.java
+++ src/main/testClassJunit.java
@@ -1,2 +1,13 @@
+import org.junit.Test;
+import org.joda.time.DateTime;
 public class testClassJunit {
+
+    @Test
+
+    studentClass student = new studentClass("Maria Henehan", 21, , String ID, ArrayList<String> courses, ArrayList<String> modules)
+
+    public void testStudent()
+    {
+        assertEquals("")
+    }
 }
